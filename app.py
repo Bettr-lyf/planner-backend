c4b822154c5e89fb1a2d8f987e3112c34c45b22b
Aayush Kurup <aayushkurup786@outlook.com>
2025-04-27 11:47:29 +0530

    ADD: bearer token authorization

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import HTTPBearer
 from routes import task, auth
 from contextlib import asynccontextmanager
 
 from config.tortoise import init_db
 
+bearer_scheme = HTTPBearer()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,9 +16,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-@app.get('/')
-def index():
-  return {'message': 'Hello World'}
+@app.get('/health', tags=['health'])
+def health():
+  return {'message': 'All izz well!!'}
 
-app.include_router(task.router, prefix='/api/v1/tasks', tags=['tasks'])
 app.include_router(auth.router, prefix='/api/v1/auth', tags=['auth'])
+app.include_router(task.router, prefix='/api/v1/tasks', tags=['tasks'], dependencies=[Depends(bearer_scheme)])
